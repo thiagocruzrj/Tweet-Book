0f8bae0e2aba914b411ef02a428f9d7a99487b6f
Thiago Cruz <thagocruz@gmail.com>
2020-03-18 12:17:40 -0300

    Create installer files configuration

---
+++ TweetBook/Installers/MvcIsntaller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TweetBook.Installers
+{
+    public class MvcIsntaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(x => { x.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "TweetBook Api", Version = "v1" }); });
+        }
+    }
+}
