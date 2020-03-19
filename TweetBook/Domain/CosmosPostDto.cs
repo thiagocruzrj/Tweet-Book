bdd74736e5449d2bd51254706e176938cd6b7384
Thiago Cruz <thagocruz@gmail.com>
2020-03-18 22:44:17 -0300

    Start post service implementation

--- TweetBook/Domain/CosmosPostDto.cs
+++ TweetBook/Domain/CosmosPostDto.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Cosmonaut.Attributes;
+using Newtonsoft.Json;
 
 namespace TweetBook.Domain
 {
+    [CosmosCollection("posts")]
     public class CosmosPostDto
     {
+        [CosmosPartitionKey]
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- TweetBook/Services/CosmosPostService.cs
+++ TweetBook/Services/CosmosPostService.cs
@@ -1,6 +1,43 @@
-﻿namespace TweetBook.Services
+﻿using Cosmonaut;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TweetBook.Domain;
+
+namespace TweetBook.Services
 {
-    public class CosmosPostService
+    public class CosmosPostService : IPostService
     {
+        private readonly ICosmosStore<CosmosPostDto> _cosmosStore;
+
+        public CosmosPostService(ICosmosStore<CosmosPostDto> cosmosStore)
+        {
+            _cosmosStore = cosmosStore;
+        }
+
+        public async Task<bool> CreatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeletePost(Guid postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> GetPostById(Guid postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Post>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdatePost(Post postUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
