0cde947bd29adaea5fe7c53387f99abede234758
Thiago Cruz <thagocruz@gmail.com>
2020-03-21 22:01:58 -0300

    Create necessary files to identity auth

--- TweetBook/Contracts/V1/ApiRoutes.cs
+++ TweetBook/Contracts/V1/ApiRoutes.cs
@@ -14,5 +14,11 @@
             public const string Delete = Base + "/posts/{postId}";
             public const string Create = Base + "/posts";
         }
+
+        public static class Identity
+        {
+            public const string Login = Base + "/identity/login";
+            public const string Register = Base + "/identity/register";
+        }
     }
 }
---
+++ TweetBook/Controllers/V1/IdentityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TweetBook.Services;
+
+namespace TweetBook.Controllers.V1
+{
+    public class IdentityController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public IdentityController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+    }
+}
