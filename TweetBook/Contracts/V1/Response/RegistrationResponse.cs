bd7df689ee67f3a65f7e07c9cf8bd05dbafd6da9
Thiago Cruz <thagocruz@gmail.com>
2020-03-21 22:10:54 -0300

    Create auth classes

---
+++ TweetBook/Contracts/V1/Response/RegistrationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace TweetBook.Contracts.V1.Response
+{
+    public class RegistrationResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ TweetBook/Domain/AuthenticationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Domain
+{
+    public class AuthenticationResult
+    {
+        public string Token { get; set; }
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
