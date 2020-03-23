1ffb82e64c26c6727ea327f6c5b367653a0ea346
Thiago Cruz <thagocruz@gmail.com>
2020-03-23 13:41:37 -0300

    Create extension claims class

---
+++ TweetBook/Extension/GeneralExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Extension
+{
+    public static class GeneralExtension
+    {
+        public static string GetUserId(this HttpContext httpContext)
+        {
+            if(httpContext == null)
+                return string.Empty;
+
+            return httpContext.User.Claims.Single(x => x.Type == "id").Value;
+        }
+    }
+}
