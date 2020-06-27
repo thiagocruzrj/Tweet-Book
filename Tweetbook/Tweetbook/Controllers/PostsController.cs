e43eeed42f13a297d77eae367964770e6c382a80
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 16:17:14 -0300

    Add new Post Domain entitiy

---
+++ Tweetbook/Tweetbook/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Controllers
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
