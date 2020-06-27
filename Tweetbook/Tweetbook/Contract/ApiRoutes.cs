baa15188f3eca0dcabc93dfdbbf7f6d9ae1ca2ca
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 16:30:35 -0300

    Add base endpoints

--- Tweetbook/Tweetbook/Contract/ApiRoutes.cs
+++ Tweetbook/Tweetbook/Contract/ApiRoutes.cs
@@ -2,11 +2,13 @@
 {
     public static class ApiRoutes
     {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public static readonly string Base = $"{Root}/{Version}";
+
         public static class Posts
         {
-            public const string GetAll = "api/v1/posts";
-            public const string Create = "api/v1/posts";
-            public const string Get = "api/v1/posts/{postId}";
+            public static readonly string GetAll = $"{Base}/posts";
         }
     }
 }
