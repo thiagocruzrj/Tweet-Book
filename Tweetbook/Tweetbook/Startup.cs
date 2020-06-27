e166657a907a107e34bc93dcb6a37857f13526d1
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 17:43:25 -0300

    Finishing installation of all required serices by configuration

--- Tweetbook/Tweetbook/Startup.cs
+++ Tweetbook/Tweetbook/Startup.cs
@@ -20,11 +20,13 @@ namespace Tweetbook
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var classesImplementingInstaller = typeof(Startup).Assembly.ExportedTypes.Where(x =>
-                                                typeof(IInstaller).IsAssignableFrom(x) 
-                                                 && !x.IsInterface && !x.IsAbstract)
-                                                  .Select(Activator.CreateInstance)
-                                                   .Cast<IInstaller>().ToList();
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
+                                typeof(IInstaller).IsAssignableFrom(x) 
+                                && !x.IsInterface && !x.IsAbstract)
+                                .Select(Activator.CreateInstance)
+                                .Cast<IInstaller>().ToList();
+
+            installers.ForEach(Installer => Installer.InstallServices(services, Configuration));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
