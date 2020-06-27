b57f8608d73053d63e87bfc4b3d4a7cca7208152
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 17:47:22 -0300

    Create InstallerExtensions.cs

---
+++ Tweetbook/Tweetbook/Installer/InstallerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Tweetbook.Installer
+{
+    public static class InstallerExtensions
+    {
+        public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
+                                typeof(IInstaller).IsAssignableFrom(x)
+                                && !x.IsInterface && !x.IsAbstract)
+                                .Select(Activator.CreateInstance)
+                                .Cast<IInstaller>().ToList();
+
+            installers.ForEach(Installer => Installer.InstallServices(services, configuration));
+        }
+    }
+}
