78cb5db47231ef52a0872767e3d25f1f1c90fab8
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 17:48:05 -0300

    register registration service

--- Tweetbook/Tweetbook/Startup.cs
+++ Tweetbook/Tweetbook/Startup.cs
@@ -20,13 +20,7 @@ namespace Tweetbook
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
-                                typeof(IInstaller).IsAssignableFrom(x) 
-                                && !x.IsInterface && !x.IsAbstract)
-                                .Select(Activator.CreateInstance)
-                                .Cast<IInstaller>().ToList();
-
-            installers.ForEach(Installer => Installer.InstallServices(services, Configuration));
+            services.InstallServicesInAssembly(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
