308fc63071fb0263a6a003d3bac1be057290eef2
Thiago Cruz <thagocruz@gmail.com>
2020-06-27 16:26:54 -0300

    Add new routes static class

---
+++ Tweetbook/Tweetbook/Contract/ApiRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace Tweetbook.Contract
+{
+    public static class ApiRoutes
+    {
+        public static class Posts
+        {
+            public const string GetAll = "api/v1/posts";
+            public const string Create = "api/v1/posts";
+            public const string Get = "api/v1/posts/{postId}";
+        }
+    }
+}
