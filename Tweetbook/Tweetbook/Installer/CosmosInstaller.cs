bbdc9d71b79fbb0c7cf84c2e8576bf3245496c47
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 17:37:50 -0300

    Create CosmosInstaller.cs

---
+++ Tweetbook/Tweetbook/Installer/CosmosInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Cosmonaut;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tweetbook.Installer
+{
+    public class CosmosInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var cosmosStoreSettings = new CosmosStoreSettings(
+                configuration["CosmosSettings:DatabaseName"],
+                configuration["CosmosSettings:AccountUri"],
+                configuration["CosmosSettings:AccountKey"]);
+        }
+    }
+}
