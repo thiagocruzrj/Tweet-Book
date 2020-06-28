884b8fc7dc24d619f30cac3a31970a1f072b57bd
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 17:32:18 -0300

    Create CosmosPostService.cs

---
+++ Tweetbook/Tweetbook/Services/CosmosPostService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Services
+{
+    public class CosmosPostService : IPostService
+    {
+        public Task<List<Post>> GetPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> CreatePostAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePostAsync(Post postToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePostAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
