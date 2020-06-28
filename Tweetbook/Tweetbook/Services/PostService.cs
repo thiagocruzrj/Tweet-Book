6b5ee04449f0b501f82f6bad8013ba32c7f4c9a1
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 16:10:52 -0300

    Rename service methods

--- Tweetbook/Tweetbook/Services/PostService.cs
+++ Tweetbook/Tweetbook/Services/PostService.cs
@@ -16,33 +16,33 @@ namespace Tweetbook.Services
             _dataContext = dataContext;
         }
 
-        public async Task<Post> GetPostById(Guid id)
+        public async Task<Post> GetPostByIdAsync(Guid id)
         {
             return await _dataContext.Posts.SingleOrDefaultAsync(x => x.Id == id);
         }
 
-        public async Task<List<Post>> GetPosts()
+        public async Task<List<Post>> GetPostsAsync()
         {
             return await _dataContext.Posts.ToListAsync();
         }
 
-        public async Task<bool> CreatePost(Post post)
+        public async Task<bool> CreatePostAsync(Post post)
         {
             await _dataContext.Posts.AddAsync(post);
             var created = await _dataContext.SaveChangesAsync();
             return created > 0;
         }
 
-        public async Task<bool> UpdatePost(Post postToUpdate)
+        public async Task<bool> UpdatePostAsync(Post postToUpdate)
         {
             _dataContext.Posts.Update(postToUpdate);
             var updated = await _dataContext.SaveChangesAsync();
             return updated > 0;
         }
 
-        public async Task<bool> DeletePost(Guid id)
+        public async Task<bool> DeletePostAsync(Guid id)
         {
-            var post = await GetPostById(id);
+            var post = await GetPostByIdAsync(id);
             _dataContext.Posts.Remove(post);
             var deleted = await _dataContext.SaveChangesAsync();
             return deleted > 0;
