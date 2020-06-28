4e4bcb1edebdb856c2f1d7428192eeac2ebe9ae3
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 14:34:29 -0300

    Refector controller with new post service DI

--- Tweetbook/Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Tweetbook/Controllers/V1/PostsController.cs
@@ -1,46 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Tweetbook.Contract.V1;
 using Tweetbook.Controllers.Responses;
 using Tweetbook.Controllers.V1.Requests;
 using Tweetbook.Domain;
+using Tweetbook.Services;
 
 namespace Tweetbook.Controllers.V1
 {
     public class PostsController : Controller
     {
-        private readonly List<Post> _posts;
+        private IPostService _postService;
 
-        public PostsController()
+        public PostsController(IPostService postService)
         {
-            _posts = new List<Post>();
-            for (int i = 0; i < 5; i++)
-            {
-                _posts.Add(new Post 
-                {
-                    Id = Guid.NewGuid(),
-                    Name = $"Post Name {i}"
-                });
-            }
+            _postService = postService;
         }
 
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
-            return Ok(_posts);
+            return Ok(_postService.GetPosts());
         }
 
         [HttpGet(ApiRoutes.Posts.Get)]
         public IActionResult Get([FromRoute]Guid postId)
         {
-            var post = _posts.SingleOrDefault(x => x.Id == postId);
+            var post = _postService.GetPostById(postId);
 
             if (post == null)
                 return NotFound();
 
-            return Ok(_posts);
+            return Ok(post);
         }
 
         [HttpPost(ApiRoutes.Posts.Create)]
@@ -51,7 +43,7 @@ namespace Tweetbook.Controllers.V1
             if (post.Id != Guid.Empty)
                 post.Id = Guid.NewGuid();
 
-            _posts.Add(post);
+            _postService.GetPosts().Add(post);
 
             var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
             var locationUrl = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
