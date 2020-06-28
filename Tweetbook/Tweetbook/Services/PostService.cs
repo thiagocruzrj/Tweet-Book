97ec0822b5e738f0e5e37b11545bc509b8515de0
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 14:26:42 -0300

    Start post service implementation

---
+++ Tweetbook/Tweetbook/Services/PostService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Services
+{
+    public class PostService : IPostService
+    {
+        public Post GetPostById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
