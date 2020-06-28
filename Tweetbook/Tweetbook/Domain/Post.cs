6a7277ba4eb6366bb9e355a25400fdbf9e5dadde
Thiago Cruz <thagocruz@gmail.com>
2020-06-28 15:36:49 -0300

    Add new dbset

--- Tweetbook/Tweetbook/Domain/Post.cs
+++ Tweetbook/Tweetbook/Domain/Post.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tweetbook.Domain
 {
     public class Post
     {
+        [Key]
         public Guid Id { get; set; }
+
         public string Name { get; set; }
     }
 }
