a36dfc583e6e1e9ac66164f019216b6db55bd537
Thiago Cruz <thagocruz@gmail.com>
2020-06-29 19:10:06 -0300

    Add bad request when model state is invalid

--- Tweetbook/Controllers/V1/IdentityController.cs
+++ Tweetbook/Controllers/V1/IdentityController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 using Tweetbook.Contract.V1;
 using Tweetbook.Controllers.Responses;
@@ -19,6 +20,14 @@ namespace Tweetbook.Controllers.V1
         [HttpPost(ApiRoutes.Identity.Register)]
         public async Task<IActionResult> Register([FromBody] UserRegistrationRequest request)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage))
+                });
+            }
+
             var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
 
             if(!authResponse.Success)
--- Tweetbook/Controllers/V1/Requests/UserRegistrationRequest.cs
+++ Tweetbook/Controllers/V1/Requests/UserRegistrationRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Tweetbook.Controllers.V1.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tweetbook.Controllers.V1.Requests
 {
     public class UserRegistrationRequest
     {
+        [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }
     }
