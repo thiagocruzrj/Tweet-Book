976ec375ce6147231ead213d2c4b5e95f2f70346
Thiago Cruz <thagocruz@gmail.com>
2020-06-30 23:42:43 -0300

    Add new migration

--- Tweetbook/Data/ApplicationDbContext.cs
+++ Tweetbook/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace Tweetbook.Data
             : base(options) { }
 
         public DbSet<Post> Posts { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
