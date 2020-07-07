fbc22bfbad7708d81dbde0ae606c3500ddf79060
Thiago Cruz <thagocruz@gmail.com>
2020-07-07 00:11:25 -0300

    Starting with post controller

---
+++ Tests/Tweetbook.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        private readonly HttpClient TestClient;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
---
+++ Tests/Tweetbook.IntegrationTests/PostControllerTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Tweetbook.IntegrationTests
+{
+    public class PostControllerTest : IntegrationTest
+    {
+
+    }
+}
