188f19bc2ee3dd8e777b66556f5ad0d53328558b
Thiago Cruz <thagocruz@gmail.com>
2020-07-07 00:41:43 -0300

    Finish auth implementation test

--- Tests/Tweetbook.IntegrationTests/IntegrationTest.cs
+++ Tests/Tweetbook.IntegrationTests/IntegrationTest.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Tweetbook.Contract.V1;
+using Tweetbook.Controllers.Responses;
+using Tweetbook.Controllers.V1.Requests;
+using Tweetbook.Data;
 
 namespace Tweetbook.IntegrationTests
 {
@@ -11,13 +16,37 @@ namespace Tweetbook.IntegrationTests
 
         protected IntegrationTest()
         {
-            var appFactory = new WebApplicationFactory<Startup>();
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(o =>
+                        {
+                            o.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
             TestClient = appFactory.CreateClient();
         }
 
         protected async Task AuthenticateAsync()
         {
-            TestClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("bearer", GetJwtAsync());
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "teste@integration.com",
+                Password = "Test@123."
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSucessResponse>();
+            return registrationResponse.Token;
         }
     }
 }
