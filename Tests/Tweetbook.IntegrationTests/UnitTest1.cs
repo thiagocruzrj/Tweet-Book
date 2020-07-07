5af124563af6e6f5e222eb0da3e43bc476fa87df
Thiago Cruz <thagocruz@gmail.com>
2020-07-07 00:04:18 -0300

    Create UnitTest1.cs

---
+++ Tests/Tweetbook.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using Xunit;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class UnitTest1
+    {
+        private readonly HttpClient _client;
+
+        public UnitTest1()
+        {
+            var appFactory = new WebApplicationFactory<Startup>().CreateClient();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
