b77b042677c4a890ea0c0367ba98d323c84d25e4
Thiago Cruz <thagocruz@gmail.com>
2020-07-08 22:13:35 -0300

    Add tag controller

---
+++ Tweetbook/Controllers/V1/TagsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Services;
+
+namespace Tweetbook.Controllers.V1
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class TagsController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public TagsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+    }
+}
