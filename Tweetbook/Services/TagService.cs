eb4349889ae16d6acb06d2920e4b9da54d2a84c2
Thiago Cruz <thagocruz@gmail.com>
2020-07-08 22:26:03 -0300

    Add CreateTagAsync method

--- Tweetbook/Services/TagService.cs
+++ Tweetbook/Services/TagService.cs
@@ -21,5 +21,17 @@ namespace Tweetbook.Services
         {
             return await _dataContext.Tags.AsNoTracking().ToListAsync();
         }
+
+        public async Task<bool> CreateTagAsync(Tag tag)
+        {
+            tag.Name = tag.Name.ToLower();
+            var existingTag = await _dataContext.Tags.AsNoTracking().SingleOrDefaultAsync(x => x.Name == tag.Name);
+            if (existingTag != null)
+                return true;
+
+            await _dataContext.Tags.AddAsync(tag);
+            var created = await _dataContext.SaveChangesAsync();
+            return created > 0;
+        }
     }
 }
