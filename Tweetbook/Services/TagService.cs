5cf0331ef095ed3ebc6ff11fd9966ec591c257f2
Thiago Cruz <thagocruz@gmail.com>
2020-07-08 22:25:21 -0300

    Add new tag service

---
+++ Tweetbook/Services/TagService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetbook.Data;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly DataContext _dataContext;
+
+        public TagService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
